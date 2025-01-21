d30cf34079416db9bd26325b0acda9fc4b8fc2da
Vítor Jun Kokitsu <106423043+JunKokitsu@users.noreply.github.com>
2025-01-21 20:05:50 -0300

    Adicionando a questão 1
    -Criação do projeto Scrapy.
    -Desenvolvimento do Script de extração dos dados.

---
+++ Questao_1/src/compra_agora_scrapy/compra_agora_scrapy/spiders/main.py
@@ -0,0 +1,10 @@
+from scrapy.crawler import CrawlerProcess
+from compra_agora_spider import CompraAgoraSpiderSpider
+from scrapy.utils.project import get_project_settings
+if __name__ == "__main__":
+    """
+    Arquivo principal para executar o spider.
+    """
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(CompraAgoraSpiderSpider)
+    process.start()
